359f73b4617612517edc254170ed0cbe0c1a2279
Dobriyan Petkov <dobriyanp@yahoo.com>
2021-05-26 18:43:27 +0100

    rewrited some functions

--- condenseArrayToNumber.js
+++ condenseArrayToNumber.js
@@ -1,13 +1,10 @@
 function condenseArrayToNumber(nums) {
 
     while (nums.length > 1) { 
-        condenseArray(nums);
-    }
 
-    function condenseArray(array) {
         let condensedArray = [];
-        for (let i = 0; i < array.length - 1; i++) {
-            condensedArray[i] = array[i] + array[i+1];
+        for (let i = 0; i < nums.length - 1; i++) {
+            condensedArray[i] = nums[i] + nums[i+1];
         }
         nums = condensedArray;
     }
@@ -16,4 +13,4 @@ function condenseArrayToNumber(nums) {
     console.log(nums[0]);
 }
 
-condenseArrayToNumber([1]);
+condenseArrayToNumber([5,0,4,1,2]);
--- rightPlace.js
+++ rightPlace.js
@@ -1,9 +1,32 @@
 function rightPlace(stringWithMissingChar, missingChar, stringToCompareWith) {
     correctedString = stringWithMissingChar.replace('_', missingChar);
 
-    result = correctedString === stringToCompareWith ? 'Matched' : 'Not Matched';
-
+    result = correctedString === stringToCompareWith 
+        ? 'Matched' 
+        : 'Not Matched';
+    
+    console.log('1----------')
     console.log(result);
+    
 }
 
 rightPlace('Str_ng', 'i', 'String');
+
+
+function rightPlace2(charMissingString, char, compareString) {
+    let finalString = '';
+    for (i = 0; i < charMissingString.length; i++) {
+        charMissingString[i] === '_' 
+        ? finalString += char 
+        : finalString += charMissingString[i];
+    }
+
+    result = finalString === compareString 
+        ? 'Matched' 
+        : 'Not Matched';
+
+    console.log('2----------')
+    console.log(result)
+}
+
+rightPlace2('Str_ng', 'i', 'Strong');
